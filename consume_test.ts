3ca2bd1fd2679fcbd91347cd2166aeca66126646
NDH <nick@ndhrones.com>
2023-12-07 21:44:52 -0500

    Update consume_test.ts

--- consume_test.ts
+++ consume_test.ts
@@ -5,9 +5,9 @@ import { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
 Deno.test("consume", () => {
    const acc = new Accumulator()
    acc.appendBuffer( Uint8Array.from([1,2,3,4]) )
-   console.log('insertionPoint ', acc.insertionPoint)
-   acc.consume(2)
-   console.log('insertionPoint ', acc.insertionPoint)
+   console.log('insertionPoint ', acc.insertionPoint) // insertionPoint  4
+   acc.consume(2) // remove first two bytes
+   console.log('insertionPoint ', acc.insertionPoint) // insertionPoint  2
    assertEquals( acc.insertionPoint, 2)
-   console.log('value ', acc.extract())
+   console.log('value ', acc.extract()) // Uint8Array(2) [ 3, 4 ]
 })
